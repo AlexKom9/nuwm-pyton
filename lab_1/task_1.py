754958cf43ccb027c70c17b1eaf556b7cc89ad4f
alex <909komar@gmail.com>
2023-04-09 16:57:06 +0300

    lab_1 task_1

---
+++ lab_1/task_1.py
@@ -0,0 +1,9 @@
+teacher_name = input("Enter teacher full name: ")
+student_name = input("Enter student full name: ")
+student_group = input("Enter student group: ")
+
+result = f"""
+Hello {teacher_name}! My name is {student_name}, group {student_group}.
+"""
+
+print(result)
